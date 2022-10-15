da26534bbc13ca5eec65208616454b0e67eb3973
josemar brito <estudos.josemarbrito@gmail.com>
2022-10-15 01:22:45 -0300

    Criado Base da pagina HTML close #2

--- flix/settings.py
+++ flix/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'flix.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,10 +123,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = 'media/'
-    
 MEDIA_ROOT = BASE_DIR / 'media'
-
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
